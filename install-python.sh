bf2a8d2e76e9c2924752a1550dd708355f142cf5
AhmedAbozeid622 <49274489+AhmedAbozeid622@users.noreply.github.com>
2021-06-03 20:10:25 +0200

    Update install-python.sh

--- install-python.sh
+++ install-python.sh
@@ -8,3 +8,17 @@ cd /usr/lib/python3.6 && wget https://bootstrap.pypa.io/get-pip.py && python3.6 
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
 ./ngrok authtoken 1nQmkjYRvHxcSCQIlhrDBtxenYD_3pZ5g3KjUuVHeLaYn9R9G
+
+
+# install Jupyter & create config file 
+sudo pip install jupyter
+jupyter notebook --generate-config
+echo "c.NotebookApp.allow_remote_access = True" >>  /root/.jupyter/jupyter_notebook_config.py # Allow remote access (i.e: ngrok , remote.it , ...)
+jupyter notebook password # Must enter password
+
+
+### (Optional) ###
+# jupyter notebook </dev/null &>/dev/null &  ### in the backend
+# nohup ./ngrok http 8888 &>/dev/null &
+
+
