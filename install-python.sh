f5f70d0619660078871e6c33601b0c692f87e411
AhmedAbozeid622 <49274489+AhmedAbozeid622@users.noreply.github.com>
2021-06-03 20:49:36 +0200

    Update install-python.sh

--- install-python.sh
+++ install-python.sh
@@ -5,9 +5,9 @@ sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
 sudo apt-get install python3.6 -y 
 cd /usr/lib/python3.6 && wget https://bootstrap.pypa.io/get-pip.py && python3.6 get-pip.py
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ngrok-stable-linux-amd64.zip
-./ngrok authtoken 1nQmkjYRvHxcSCQIlhrDBtxenYD_3pZ5g3KjUuVHeLaYn9R9G
+curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -o /root/ngrok-stable-linux-amd64.zip
+unzip /root/ngrok-stable-linux-amd64.zip
+/root/ngrok authtoken 1nQmkjYRvHxcSCQIlhrDBtxenYD_3pZ5g3KjUuVHeLaYn9R9G
 
 
 # install Jupyter & create config file 
